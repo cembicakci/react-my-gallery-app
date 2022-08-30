73c2f50ad46b4c93f3de04f469e694653f23d738
Cem <cmbicakci@gmail.com>
2022-08-30 19:32:31 +0300

    installed firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBs_K3EuX0_5t47zlnLnOwlKXxEf7bFuEA",
+    authDomain: "react-my-gallery-5efd1.firebaseapp.com",
+    projectId: "react-my-gallery-5efd1",
+    storageBucket: "react-my-gallery-5efd1.appspot.com",
+    messagingSenderId: "459587627488",
+    appId: "1:459587627488:web:f4a95fbf1c7d7295806385"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
