adc1f338bb58d4f608986bead108a20fae129feb
Cem <cmbicakci@gmail.com>
2022-08-31 19:58:41 +0300

    uploaded images from firestore

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import useFirestore from '../hooks/useFirestore'
 
 function ImageGrid() {
-  return (
-    <div className='image-grid'>ImageGrid</div>
-  )
+    const { docs } = useFirestore('images')
+    console.log(docs)
+
+    return (
+        <div className='image-grid'>
+            {docs &&  docs.map(doc => (
+                <div className='img-wrap' key={doc.id}>
+                    <img src={doc.url} alt='uploaded pic' />
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default ImageGrid
