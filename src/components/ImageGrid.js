0544e7ceb4e65bdaaeb29b4824ac293a0e63db21
Cem <cmbicakci@gmail.com>
2022-08-31 20:01:08 +0300

    styled image grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -6,7 +6,7 @@ function ImageGrid() {
     console.log(docs)
 
     return (
-        <div className='image-grid'>
+        <div className='img-grid'>
             {docs &&  docs.map(doc => (
                 <div className='img-wrap' key={doc.id}>
                     <img src={doc.url} alt='uploaded pic' />
