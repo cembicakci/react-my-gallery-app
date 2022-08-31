4cd90d3cdf6f3ffa4b92af764ad6fcfda914563d
Cem <cmbicakci@gmail.com>
2022-08-31 20:52:21 +0300

    added framer-motion

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import useFirestore from '../hooks/useFirestore'
+import { motion } from 'framer-motion'
 
 function ImageGrid({ setSelectedImg }) {
     const { docs } = useFirestore('images')
@@ -8,9 +9,9 @@ function ImageGrid({ setSelectedImg }) {
     return (
         <div className='img-grid'>
             {docs && docs.map(doc => (
-                <div className='img-wrap' key={doc.id} onClick={() => setSelectedImg(doc.url)}>
-                    <img src={doc.url} alt='uploaded pic' />
-                </div>
+                <motion.div layout whileHover={{ opacity: 1 }} className='img-wrap' key={doc.id} onClick={() => setSelectedImg(doc.url)}>
+                    <motion.img src={doc.url} alt='uploaded pic' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{delay: 1}} />
+                </motion.div>
             ))}
         </div>
     )
