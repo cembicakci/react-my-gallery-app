e3c4214d0e91b73c465def8187dfc1719458ae2c
Cem <cmbicakci@gmail.com>
2022-08-31 21:01:37 +0300

    Updates

---
+++ precache-manifest.0cb779bd2b537de3668bfcf51df70d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e11f0cd37c3c3417ede1a027bdd943",
+    "url": "/react-my-gallery-app/index.html"
+  },
+  {
+    "revision": "384d04a5c4c2d068d3db",
+    "url": "/react-my-gallery-app/static/css/main.0515658a.chunk.css"
+  },
+  {
+    "revision": "8c0b0f8bdd60406fe6f8",
+    "url": "/react-my-gallery-app/static/js/2.89e3c0d2.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/react-my-gallery-app/static/js/2.89e3c0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384d04a5c4c2d068d3db",
+    "url": "/react-my-gallery-app/static/js/main.7734dd54.chunk.js"
+  },
+  {
+    "revision": "aec52b233e1c0cea293d",
+    "url": "/react-my-gallery-app/static/js/runtime-main.501ab8bd.js"
+  }
+]);
